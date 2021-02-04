659572c9fe3712d8225a5a2fb996feae98a071eb
SmokeySam <rossanthonyphelan@gmail.com>
2021-02-04 21:43:04 +0000

    updated comments and grammer checks

--- README.md
+++ README.md
@@ -2,17 +2,10 @@
 
 I chose to do my second project on a webpage that shows dog friendly restraunts in Ireland.
 
+
 # UX
 
-## User Goals
-
-I wanted the user to be able to find dog freindly restraunts in ireland and be able to see some infromation about them and contact them.
-
-## Developer Goals
-
-As a developer i wanted to creat a information based webpage and implament googles api maps.
-
-# Planes Of Scope
+## Planes Of Scope
 
 **Stratagy Plane**
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Google Maps Api , Waterford
+
 function initMap() {
   const myLatLng = {lat: 52.25833, lng: -7.11194};
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -5,21 +7,24 @@ function initMap() {
     center: myLatLng,
   });
 
- var marker = new google.maps.Marker({
+ // Loko Marker
+ var marker = new google.maps.Marker({                  
       position: {lat: 52.24718, lng: -7.08307},
       map: map,
       title: 'Here Is Loko!'
     });
 
-    var marker2 = new google.maps.Marker({
+ // 360 Cookhouse Marker
+ var marker2 = new google.maps.Marker({
       position: {lat: 52.08911, lng: -7.61723},
       map: map,
       title: 'Here is 360 Cookhouse!'
     });
 
-    var marker2 = new google.maps.Marker({
+ // Dog House Marker
+ var marker3 = new google.maps.Marker({
       position: {lat: 53.38897, lng: -6.07470},
       map: map,
       title: 'Here is The Dog House!'
     });
-}
+} 
